008fa83bb03b98c0d1d47b8db6749125261e176f
Jems_Javiya <javiyajems@gmail.com>
2025-06-19 13:07:08 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -18,16 +18,10 @@ app.use(express.json());
 
 // MongoDB Atlas connection
 mongoose
-  .connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('MongoDB connected');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 // Sample schema
 const UserSchema = new mongoose.Schema({
